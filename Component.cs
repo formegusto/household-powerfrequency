418516658a3a7be339314855f0537c6282be9110
formegusto <formegusto@gmail.com>
2021-05-10 16:24:09 +0900

    쓰레드 관리 어렵다용..

--- Component.cs
+++ Component.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroUI.Common;
@@ -13,6 +14,7 @@ using LiveCharts;
 using LiveCharts.Defaults;
 using LiveCharts.Wpf;
 using LiveCharts.WinForms;
+using System.Windows.Threading;
 
 using MetroUI.Entity;
 
@@ -25,50 +27,6 @@ namespace MetroUI
 		public Component()
 		{
 			InitializeComponent();
-			ChartValues<ObservablePoint> test = new ChartValues<ObservablePoint>();
-			this.Chart.Series = new SeriesCollection
-			{
-				new LineSeries
-				{
-					Title = "0~3 Power Frequency",
-					Values = new ChartValues<ObservablePoint>
-					{
-						new ObservablePoint(1,5),
-						new ObservablePoint(1.5,7.6),
-						new ObservablePoint(2,21),
-						new ObservablePoint(5,25),
-						new ObservablePoint(10,30),
-						new ObservablePoint(17,30),
-						new ObservablePoint(19.6,30),
-						new ObservablePoint(30,40),
-					}
-				},
-				new LineSeries
-				{
-					Title = "3~6 Power Frequency",
-					Values = new ChartValues<ObservablePoint>
-					{
-						new ObservablePoint(5,5),
-						new ObservablePoint(6.5,7.6),
-						new ObservablePoint(7,21),
-						new ObservablePoint(19.6,30),
-						new ObservablePoint(30,40),
-					}
-				},
-			};
-
-			this.Chart.AxisX = new AxesCollection()
-			{
-				new LiveCharts.Wpf.Axis()
-				{
-					Title= "Power",
-					Separator = new LiveCharts.Wpf.Separator()
-					{
-						Step = 5.0,
-						IsEnabled = false
-					}
-				}
-			};
 		}
 		public void SetController(IController controller)
 		{
@@ -97,8 +55,11 @@ namespace MetroUI
 					this.Chart.AxisX.Clear();
 					this.Chart.Series.Clear();
 					this.ChartContainer.Controls.Add(this.Chart);
-					ConfigChart(e.powerFrequencies);
-
+					Task.Run(() =>
+					{
+						ConfigChart(e.powerFrequencies);
+					});
+					
 					break;
 				default:
 					break;
@@ -106,21 +67,25 @@ namespace MetroUI
 		}
 		public void ConfigChart(List<PowerFrequency>[] pf)
 		{
-			int startHours = 0;
-			for(int p = 0; p < pf.Length; p++)
-			{
-				ChartValues<ObservablePoint> cv = new ChartValues<ObservablePoint>();
-				pf[p].ForEach((pp) =>
+			this.Invoke((System.Action)( () => {
+				int startHours = 0;
+				for (int p = 0; p < pf.Length; p++)
 				{
-					cv.Add(new ObservablePoint(pp.wh, pp.frequency));
-				});
-				LineSeries ls = new LineSeries
-				{
-					Title = string.Format("{0}~{1}h PowerFrequency", startHours, startHours += 3),
-					Values = cv
-				};
-				this.Chart.Series.Add(ls);
-			}
+
+					ChartValues<ObservablePoint> cv = new ChartValues<ObservablePoint>();
+					pf[p].ForEach((pp) =>
+					{
+						cv.Add(new ObservablePoint(pp.wh, pp.frequency));
+					});
+
+					LineSeries ls = new LineSeries
+					{
+						Title = string.Format("{0}~{1}h Power Frequency", startHours, startHours += 3),
+						Values = cv
+					};
+					this.Chart.Series.Add(ls);
+				}
+			}));
 		}
 		private void LoadBtn_Click(object sender, EventArgs e) => this.controller.Dispatch(MODEL_ACTIONS.LOAD_EXCEL);
 		private void UIDSearch_Changed(object sender, EventArgs e) => this.changed(this, new ViewEventArgs(VIEW_ACTIONS.CHANGE_KEYWORD, this.UIDSearch.Text));
